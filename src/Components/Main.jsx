7ab5753671b9f051ece3f42a53254b859e3899a0
Biswajit9609 <biswajitchatterjee347@gmail.com>
2025-05-17 14:25:47 +0530

    css modifications

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -16,10 +16,10 @@ function Main() {
     <main>
         <form
         action={addIngredients}
-        className='h-[2.2rem] mt-10 m-7 flex justify-center items-center gap-3'
+        className='h-[2.2rem] w-full mt-10 my-7 px-[3rem] flex justify-center items-center gap-3'
         >
             <input
-            className='bg-white h-full flex grow-[1] w-[200px] sm:max-w-[20rem] sm:min-w[200px] border-2 border-[#ced4da] rounded-[0.3rem] px-2' 
+            className='bg-white h-full flex grow-[1] w-[180px] sm:max-w-[20rem] sm:min-w[200px] border-2 border-[#ced4da] rounded-[0.3rem] px-2' 
             placeholder='e.g. Bread' 
             type="text" 
             name="hello"
@@ -31,12 +31,12 @@ function Main() {
             <ul  className='list-disc list-inside w-full mt-5' >
                 {showIncredients}
             </ul>
-            <div className='getRecipe h-30 p-5 my-10 mx-5 rounded-[0.5rem] bg-[#F0EFEB] flex justify-center items-center gap-0'>
-                <div>
-                    <h2 className='font-semibold text-[1.2rem] mb-2'>Ready for a recipe?</h2>
-                    <p className='text-[0.7rem] w-[95%]'>Generate a recipe from your list of ingredients.</p>
+            <div className='getRecipe h-25 px-5 py-0 my-10 mx-5 rounded-[0.5rem] bg-[#F0EFEB] flex items-center gap-3'>
+                <div className='w-[60%]'>
+                    <h2 className='font-semibold text-[1rem] mb-1'>Ready for a recipe?</h2>
+                    <p className='text-[0.7rem] w-[85%]'>Generate a recipe from your list of ingredients.</p>
                 </div>
-                <button className='text-white font-bold px-3 py-2 bg-[#D17557] rounded-[0.3rem]'>Get a recipe</button>
+                <button className='flex text-[0.8rem] text-white font-bold px-3 py-2 bg-[#D17557] rounded-[0.3rem]'>Get a recipe</button>
             </div>
         </section>:null}
     </main>
